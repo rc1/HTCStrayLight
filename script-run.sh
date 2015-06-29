5ee5bfabb003b2eab9ce72f285a2a4406bf53f75
Ross Cairns <ross@electricglen.com>
2015-06-29 16:26:34 +0100

    Mutlisystem NVM

--- script-run.sh
+++ script-run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-source $(brew --prefix nvm)/nvm.sh
-nvm use
+if [ "$(uname)" == "Darwin" ]; then
+    echo "System: OSX"
+    source $(brew --prefix nvm)/nvm.sh
+    nvm use
+elif [[ $OSTYPE == "linux-gnueabihf" ]]; then
+    # Do something under Mac OS X platform
+    echo "System: Raspberry Pi 2"
+elif [ "$(uname)" == "Linux" ]; then
+    echo "System: Linux"
+fi
 
 PORT=7080 \
 IS_LOCAL=1 \
