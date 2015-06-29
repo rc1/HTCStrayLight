368fe5821ef3b5d0e2c7bb5f9b0e2723fa4a2a4e
Ross Cairns <r@theworkers.net>
2015-06-29 17:54:29 +0000

    Added motor-control with relay control and test sequence

--- app.motor-controller.js
+++ app.motor-controller.js
@@ -2,27 +2,13 @@
 // =============
 // Client application to control the motors
 
-// Modules
-// =======
-var pfio = require( 'piface-node' );
+var MotorControl = require( './lib/motor-control' );
 
-//pfio.digital_write ( 0, 0 ); // (pin, state)
 
-var relays = [ false, false, false, false,
-               false, false, false, false ];
-var idx = -1;
-var doFor = 100;
-
-(function loop () {
-    if ( --doFor >= 0 ) { setTimeout( loop, 10 ); }
-    if ( ++idx >= relays.length ) { idx = 0; }
-    relays[ idx ] = !relays[ idx ];
-    
-    console.log( 'setting', idx, 'to', relays[ idx ] ? 1 : 0 );
-    
-    pfio.init();
-    pfio.digital_write ( idx, relays[ idx ] ? 1 : 0 );
-    pfio.deinit();
-}());
-
+MotorControl
+    .init( MotorControl.make() )
+    .success( function ( motorControl ) {
+        
+        MotorControl.doTestSequence( motorControl );
+    });
 
